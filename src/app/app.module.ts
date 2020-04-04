d6293e5862eb0bd4a1c1476d6d65dc59976caf12
hasalfernando <witty.leo98@gmail.com>
2020-04-04 23:37:53 +0530

    Added new template

---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule } from '@angular/router';
+import { AppRoutingModule } from './app.routing';
+
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { FooterComponent } from './shared/footer/footer.component';
+
+import { ComponentsModule } from './components/components.module';
+import { ExamplesModule } from './examples/examples.module';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    NgbModule,
+    FormsModule,
+    RouterModule,
+    ComponentsModule,
+    ExamplesModule,
+    AppRoutingModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/app.routing.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { CommonModule, } from '@angular/common';
+import { BrowserModule  } from '@angular/platform-browser';
+import { Routes, RouterModule } from '@angular/router';
+
+import { ComponentsComponent } from './components/components.component';
+import { ProfileComponent } from './examples/profile/profile.component';
+import { SignupComponent } from './examples/signup/signup.component';
+import { LandingComponent } from './examples/landing/landing.component';
+import { NucleoiconsComponent } from './components/nucleoicons/nucleoicons.component';
+
+const routes: Routes =[
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home',             component: ComponentsComponent },
+    { path: 'user-profile',     component: ProfileComponent },
+    { path: 'signup',           component: SignupComponent },
+    { path: 'landing',          component: LandingComponent },
+    { path: 'nucleoicons',      component: NucleoiconsComponent }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    BrowserModule,
+    RouterModule.forRoot(routes,{
+      useHash: true
+    })
+  ],
+  exports: [
+  ],
+})
+export class AppRoutingModule { }
