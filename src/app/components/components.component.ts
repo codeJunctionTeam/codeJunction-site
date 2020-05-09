4c36e578783ca609640a39a8b86c2d4675137df5
rajitha1998 <rajitha1998@gmail.com>
2020-05-09 22:00:25 +0530

    solving hound violations

--- src/app/components/components.component.ts
+++ src/app/components/components.component.ts
@@ -17,15 +17,15 @@ export class ComponentsComponent implements OnInit {
     focus;
     focus1;
     focus2;
-    date: {year: number, month: number};
+    date: { year: number, month: number };
     model: NgbDateStruct;
-    constructor( private renderer : Renderer2) {}
+    constructor(private renderer: Renderer2) { }
     isWeekend(date: NgbDateStruct) {
         const d = new Date(date.year, date.month - 1, date.day);
         return d.getDay() === 0 || d.getDay() === 6;
     }
 
-    isDisabled(date: NgbDateStruct, current: {month: number}) {
+    isDisabled(date: NgbDateStruct, current: { month: number }) {
         return date.month !== current.month;
     }
 
@@ -33,13 +33,13 @@ export class ComponentsComponent implements OnInit {
         let input_group_focus = document.getElementsByClassName('form-control');
         let input_group = document.getElementsByClassName('input-group');
         for (let i = 0; i < input_group.length; i++) {
-            input_group[i].children[0].addEventListener('focus', function (){
+            input_group[i].children[0].addEventListener('focus', function () {
                 input_group[i].classList.add('input-group-focus');
             });
-            input_group[i].children[0].addEventListener('blur', function (){
+            input_group[i].children[0].addEventListener('blur', function () {
                 input_group[i].classList.remove('input-group-focus');
             });
         }
     }
-
 }
+
